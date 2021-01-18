b8bfae0b44655a2392985d567fabff4365a2fd3a
github raamenzurururu <reku9696@gmail.com>
2021-01-18 11:32:27 +0900

    [add]上下からふわっとアニメーション

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 const targetElement = document.querySelectorAll(".animationTarget");
-console.log("画面の高さ", window.innerHeight);
+const animationTargetElements = document.querySelectorAll(".textAnimation");
+
+for (let i = 0; i < animationTargetElements.length; i++) {
+  const targetElement = animationTargetElements[i];
+  texts = targetElement.textContent;
+  textsArray = [];
+
+  targetElement.textContent = "";
+
+  for (let j = 0; j < texts.split("").length; j++) {
+    const t = texts.split("")[j];
+    if (t === " ") {
+      textsArray.push(" ");
+    } else {
+      textsArray.push(
+        '<span style="animation-delay: ' +
+          (j * 0.1 + 0.3) +
+          's;">' +
+          t +
+          "</span>"
+      );
+    }
+  }
+
+  for (let k = 0; k < textsArray.length; k++) {
+    targetElement.innerHTML += textsArray[k];
+  }
+}
+
 document.addEventListener("scroll", function () {
   for (let i = 0; i < targetElement.length; i++) {
     const getElementDistance =
